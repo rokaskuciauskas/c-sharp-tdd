a40cee4d874d17974d796ea4c035750e47757d02
Rokas Kuciauskas <rokas.kuciauskas@gmail.com>
2019-04-24 14:05:37 +0300

    Final solution

---
+++ Stack/Stack.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Stack
+{
+	public class Stack<T>
+	{
+		#region Members
+		private T[] stackArray;
+		private int maximumLength;
+		#endregion
+
+		#region Properties
+		public int Size { get; private set; }
+		#endregion
+
+		#region Constructor
+		public Stack(int length)
+		{
+			maximumLength = length;
+			stackArray = new T[length];
+		}
+		#endregion
+
+		public void Push(T value)
+		{
+			if (Size == maximumLength)
+			{
+				throw new ExceededSizeException();
+			}
+
+			stackArray[Size++] = value;
+		}
+
+		public T Pop()
+		{
+			if (Size == 0)
+			{
+				throw new ExpenditureProhibitedException();
+			}
+
+			return stackArray[--Size];
+		}
+
+		public T Peek()
+		{
+			if (Size == 0)
+			{
+				throw new ExpenditureProhibitedException();
+			}
+
+			return stackArray[Size - 1];
+		}
+
+
+	}
+}
